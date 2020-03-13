968e524d1e546b20df75458294a20b3de64e3aa8
Isaac Smith <isaacsmith215@aol.com>
2020-03-13 14:37:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  array = []
+  attendees.each do|name|
+    array.push("Hello, my name is #{name}.")
+  end
+  return array
+end
+
