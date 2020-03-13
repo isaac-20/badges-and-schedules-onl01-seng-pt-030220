af834c0fd0c7ee4f8f7b837326153dd1553371d0
Isaac Smith <isaacsmith215@aol.com>
2020-03-13 15:06:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,12 +19,12 @@ end
 
 
 def printer(attendees)
-batch_badge_creator(attendees).each do |badge|
-  puts badge
-end
-
-assign_rooms(attendees).each do |badge|
-  puts badge
-end
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
 end 
   
